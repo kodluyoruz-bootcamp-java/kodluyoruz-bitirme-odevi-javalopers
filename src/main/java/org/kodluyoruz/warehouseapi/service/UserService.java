8cfb80811ddd54a80c287b091004e34d4a083d8b
metehanaltuntas <metehanaltuntas7@gmail.com>
2021-01-18 21:27:06 +0300

    sayfalarda düzenlemeler yapıldı

--- src/main/java/org/kodluyoruz/warehouseapi/service/UserService.java
+++ src/main/java/org/kodluyoruz/warehouseapi/service/UserService.java
@@ -5,7 +5,5 @@ import org.kodluyoruz.warehouseapi.model.entites.UserEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-    //UserDetails loadUserByUsername(String username);
     UserEntity save(UserDTO userDTO);
-
 }
