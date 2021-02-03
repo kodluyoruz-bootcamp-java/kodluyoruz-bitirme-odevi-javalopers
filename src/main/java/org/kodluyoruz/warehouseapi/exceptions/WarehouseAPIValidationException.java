d073187c15d77f7cee18f8fa3b8adb6cc72c5892
Umut Burdur <69586673+UmutBurdur@users.noreply.github.com>
2021-02-03 23:40:35 +0300

    Delete src directory

--- src/main/java/org/kodluyoruz/warehouseapi/exceptions/WarehouseAPIValidationException.java
+++
@@ -1,7 +0,0 @@
-package org.kodluyoruz.warehouseapi.exceptions;
-
-public class WarehouseAPIValidationException extends WarehouseAPIBaseException{
-    public WarehouseAPIValidationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/kodluyoruz/warehouseapi/service/ProductCRUDService.java
+++
@@ -1,8 +0,0 @@
-package org.kodluyoruz.warehouseapi.service;
-
-import org.kodluyoruz.warehouseapi.base.WarehouseAPICRUDBaseService;
-import org.kodluyoruz.warehouseapi.model.dto.ProductDTO;
-
-public interface ProductCRUDService extends WarehouseAPICRUDBaseService<ProductDTO> {
-
-}
